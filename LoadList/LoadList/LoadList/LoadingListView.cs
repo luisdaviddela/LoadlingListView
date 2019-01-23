3f5cd1a3e3cea4b918e3ec44dece8a26030a2603
Luis David De La Cruz Bautista <delacruz_luis@outlook.es>
2019-01-23 10:16:17 -0600

    Solution files

---
+++ LoadList/LoadList/LoadList/LoadingListView.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+namespace LoadList
+{
+    public class LoadingListView: ListView
+    {
+        public static readonly BindableProperty LoadMoreCommandProperty = BindableProperty.Create<LoadingListView, ICommand>(bp => bp.LoadMoreCommand, default(ICommand));
+
+        public ICommand LoadMoreCommand
+        {
+            get { return (ICommand)GetValue(LoadMoreCommandProperty); }
+            set { SetValue(LoadMoreCommandProperty, value); }
+        }
+
+        public LoadingListView()
+        {
+            RegisterLayzyLoading();
+        }
+
+        public LoadingListView(ListViewCachingStrategy cachingStrategy) : base(cachingStrategy)
+        {
+            RegisterLayzyLoading();
+        }
+
+        void RegisterLayzyLoading()
+        {
+            ItemAppearing += InfiniteListView_ItemAppearing;
+        }
+
+        object lastItem;
+        void InfiniteListView_ItemAppearing(object sender, ItemVisibilityEventArgs e)
+        {
+            // if last item is in view: load more items
+            if (ItemsSource is IList items && e.Item == items[items.Count - 1])
+            {
+                if (e.Item != lastItem && LoadMoreCommand != null && LoadMoreCommand.CanExecute(null))
+                {
+                    lastItem = e.Item;
+                    LoadMoreCommand.Execute(null);
+                }
+            }
+        }
+    }
+}
---
+++ LoadList/LoadList/LoadList/LoadingViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace LoadList
+{
+    public class LoadingViewModel
+    {
+        public ObservableCollection<string> FileNames { get; }
+        = new ObservableCollection<string>();
+        public LoadingViewModel()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                FileNames.Add($"Inserted item {i}");
+            }
+        }
+    }
+}
